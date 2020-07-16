0596c6123612f4540e6d874209c393a03a82630b
vagrant <vagrant@debian10.localdomain>
2020-07-16 21:08:58 +0000

    prueba

---
+++ laboratorio5.js
@@ -0,0 +1,37 @@
+//Parte 1
+var a, b;
+a = 6;
+b = 3;
+console.log("Parte 1\n");
+console.log("Variable a = 6 y variable b = 2");
+console.log("Suma: " + (a + b));
+console.log("Resta: " + (a - b));
+console.log("Multiplicacion: " + (a * b));
+console.log("Division: " + (a / b));
+
+//Parte 2
+let x, y;
+x = "Hola";
+y = "Profe";
+console.log("\nParte 2\n");
+console.log("Variable x = Hola y variable y = Profe");
+console.log("Resultado: " + x + " " + y);
+
+//Parte 3
+const j = 3;
+const l = "John";
+console.log("\nParte 3\n");
+console.log("Variable j = 3 y variable l = John");
+console.log("j es una variable tipo " + typeof j);
+console.log("l es una variable tipo " + typeof l);
+
+//Parte 4
+var miperro = {
+    edad_meses: 11,
+    raza: "Criolla",
+    cute: true,
+    crias: {}
+};
+console.log("\nParte 4\n");
+console.log("El objeto es miperro");
+console.log(miperro);
